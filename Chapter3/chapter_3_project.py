690647d27560217828221b30a10c05bcad1ef7a2
yoyokazoo <peter.beckfield@gmail.com>
2020-02-03 15:38:45 -0800

    Chapter 3 quiz and project

---
+++ Chapter3/chapter_3_project.py
@@ -0,0 +1,45 @@
+# Write a script that takes the user's input and draws a triangle with sides of that size.  So if the user enters 5, print:
+
+#
+##
+###
+####
+#####
+
+# if they enter 10, print:
+
+#
+##
+###
+####
+#####
+######
+#######
+########
+#########
+##########
+
+# etc.  You don't have to use pound signs, use whatever you want. Here's some sample code to get you started:
+
+triangle_size = input("Enter the size of the triangle: ")
+print(triangle_size)
+
+
+# Once you have that working, write an additional script that draws pyramids.  Take user input for the number of levels of the pyramid. So if the user enters 5, print:
+
+    #
+   ###
+  #####
+ #######
+#########
+
+# if the enter 3, print:
+
+    #
+   ###
+  #####
+
+# etc.  Here's some code to get you started:
+
+pyramid_levels = input("Enter the number of levels for your pyramid: ")
+print(pyramid_levels)
